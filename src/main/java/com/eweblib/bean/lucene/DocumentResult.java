70485dccb9d9c5c1dea3ec4360040d6949b5b150
dylan <lieping.chen@yulor.com>
2015-03-13 13:50:49 +0800

    apache lucene support

--- src/main/java/com/eweblib/bean/lucene/DocumentResult.java
+++ src/main/java/com/eweblib/bean/lucene/DocumentResult.java
@@ -1,28 +1,29 @@
 package com.eweblib.bean.lucene;
 
-//import org.apache.lucene.document.Document;
-//import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.search.ScoreDoc;
+
 
 public class DocumentResult {
 
-//	private ScoreDoc scoreDoc;
-//
-//	private Document document;
-//
-//	public ScoreDoc getScoreDoc() {
-//		return scoreDoc;
-//	}
-//
-//	public void setScoreDoc(ScoreDoc scoreDoc) {
-//		this.scoreDoc = scoreDoc;
-//	}
-//
-//	public Document getDocument() {
-//		return document;
-//	}
-//
-//	public void setDocument(Document document) {
-//		this.document = document;
-//	}
+	private ScoreDoc scoreDoc;
+
+	private Document document;
+
+	public ScoreDoc getScoreDoc() {
+		return scoreDoc;
+	}
+
+	public void setScoreDoc(ScoreDoc scoreDoc) {
+		this.scoreDoc = scoreDoc;
+	}
+
+	public Document getDocument() {
+		return document;
+	}
+
+	public void setDocument(Document document) {
+		this.document = document;
+	}
 
 }

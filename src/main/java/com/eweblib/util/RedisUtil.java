ac067e91af957deb9328a5ea9521d1163a35ebbd
dylan <lieping.chen@yulor.com>
2015-10-16 13:13:47 +0800

    add redis log

--- src/main/java/com/eweblib/util/RedisUtil.java
+++ src/main/java/com/eweblib/util/RedisUtil.java
@@ -1,10 +1,14 @@
 package com.eweblib.util;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 import com.eweblib.cfg.ConfigManager;
 import com.lambdaworks.redis.RedisClient;
 import com.lambdaworks.redis.RedisConnection;
 
 public class RedisUtil {
+	public static Logger logger = LogManager.getLogger(RedisUtil.class);
 
 	private static RedisConnection<String, String> connection = null;
 
@@ -13,18 +17,18 @@ public class RedisUtil {
 		if (connection == null) {
 			RedisClient redisClient = new RedisClient(ConfigManager.getProperty("redis_server"));
 			connection = redisClient.connect();
-
 		}
 	}
 
 	public static String loadValue(String key) {
 		init();
-		return connection.get(key);
+		String value = connection.get(key);
+		logger.info("load cache from redis:  " + key + " = " + value);
+		return value;
 	}
 
 	public static Integer loadInt(String key) {
-		init();
-		String value = connection.get(key);
+		String value = loadValue(key);
 
 		if (EweblibUtil.isValid(value)) {
 
@@ -36,6 +40,7 @@ public class RedisUtil {
 	public static void set(String key, Object value) {
 		init();
 		if (value != null) {
+			logger.info("save cache into redis:  " + key + " = " + value);
 			connection.set(key, value.toString());
 		}
 	}

9b3b34a3f9695e5229a820dabbb3f7df00e23f25
dylan <lieping.chen@yulor.com>
2015-11-03 16:55:46 +0800

    remove unused jars

--- src/main/java/com/eweblib/util/HttpClientUtil.java
+++ src/main/java/com/eweblib/util/HttpClientUtil.java
@@ -13,11 +13,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
-import org.apache.commons.httpclient.params.HttpMethodParams;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
 import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
@@ -27,14 +24,15 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.entity.mime.MultipartEntity;
-import org.apache.http.entity.mime.content.FileBody;
-import org.apache.http.entity.mime.content.StringBody;
+//import org.apache.http.entity.mime.MultipartEntity;
+//import org.apache.http.entity.mime.content.FileBody;
+//import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
 import org.apache.http.util.EntityUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -84,57 +82,57 @@ public class HttpClientUtil {
 		return null;
 
 	}
-	
-	public static void postFile(String url, String filePath, String fileName) {
-
-		HttpClient httpclient = new DefaultHttpClient();
-
-		try {
-
-			HttpPost httppost = new HttpPost(url);
-
-			FileBody bin = new FileBody(new File(filePath));
-
-			StringBody comment = new StringBody(fileName);
-
-			MultipartEntity reqEntity = new MultipartEntity();
-
-			reqEntity.addPart("file", bin);// file1为请求后台的File upload;属性
-			reqEntity.addPart("filename", comment);// filename1为请求后台的普通参数;属性
-			httppost.setEntity(reqEntity);
-
-			HttpResponse response = httpclient.execute(httppost);
-
-			int statusCode = response.getStatusLine().getStatusCode();
-
-			if (statusCode == HttpStatus.SC_OK) {
-
-				System.out.println("服务器正常响应.....");
-
-				HttpEntity resEntity = response.getEntity();
-
-				System.out.println(EntityUtils.toString(resEntity));// httpclient自带的工具类读取返回数据
-
-				System.out.println(resEntity.getContent());
-
-				EntityUtils.consume(resEntity);
-			}
-
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			try {
-				httpclient.getConnectionManager().shutdown();
-			} catch (Exception ignore) {
-
-			}
-		}
-
-	}
+//	
+//	public static void postFile(String url, String filePath, String fileName) {
+//
+//		HttpClient httpclient = new DefaultHttpClient();
+//
+//		try {
+//
+//			HttpPost httppost = new HttpPost(url);
+//
+//			FileBody bin = new FileBody(new File(filePath));
+//
+//			StringBody comment = new StringBody(fileName);
+//
+//			MultipartEntity reqEntity = new MultipartEntity();
+//
+//			reqEntity.addPart("file", bin);// file1为请求后台的File upload;属性
+//			reqEntity.addPart("filename", comment);// filename1为请求后台的普通参数;属性
+//			httppost.setEntity(reqEntity);
+//
+//			HttpResponse response = httpclient.execute(httppost);
+//
+//			int statusCode = response.getStatusLine().getStatusCode();
+//
+//			if (statusCode == HttpStatus.SC_OK) {
+//
+//				System.out.println("服务器正常响应.....");
+//
+//				HttpEntity resEntity = response.getEntity();
+//
+//				System.out.println(EntityUtils.toString(resEntity));// httpclient自带的工具类读取返回数据
+//
+//				System.out.println(resEntity.getContent());
+//
+//				EntityUtils.consume(resEntity);
+//			}
+//
+//		} catch (ParseException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} finally {
+//			try {
+//				httpclient.getConnectionManager().shutdown();
+//			} catch (Exception ignore) {
+//
+//			}
+//		}
+//
+//	}
 
 	public static String getResponseContentType(String url) {
 		HttpResponse response = doGetResponse(url, null, null, false);
@@ -208,12 +206,12 @@ public class HttpClientUtil {
 			params.setParameter("http.protocol.handle-redirects", redirect);
 
 			if (encoding != null) {
-				params.setParameter(HttpMethodParams.HTTP_CONTENT_CHARSET, encoding);
+				params.setParameter(HttpProtocolParams.HTTP_CONTENT_CHARSET, encoding);
 			}
 
-			DefaultHttpMethodRetryHandler retryHandler = new DefaultHttpMethodRetryHandler(0, false);
-			params.setParameter(HttpMethodParams.RETRY_HANDLER, retryHandler);
-			params.setParameter(HttpMethodParams.SO_TIMEOUT, 5000);
+//			HttpMethodRetryHandle retryHandler = new DefaultHttpMethodRetryHandler(0, false);
+//			params.setParameter(HttpProtocolParams.RETRY_HANDLER, retryHandler);
+			params.setParameter(HttpConnectionParams.SO_TIMEOUT, 5000);
 			HttpConnectionParams.setConnectionTimeout(params, 5000);
 		
 		    

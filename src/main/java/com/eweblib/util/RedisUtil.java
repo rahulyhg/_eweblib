e3c4593871c3c4db797e3b7feea289c5f0aefdc4
dylan <clp@winasdaq.com>
2016-03-30 22:15:44 +0800

    fix jredis issue

--- src/main/java/com/eweblib/util/RedisUtil.java
+++ src/main/java/com/eweblib/util/RedisUtil.java
@@ -10,85 +10,90 @@ import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 public class RedisUtil {
-	public static Logger logger = LogManager.getLogger(RedisUtil.class);
+    public static Logger logger = LogManager.getLogger(RedisUtil.class);
 
-	private static JedisPool jedisPool;// 非切片连接池
-	private static Jedis jedis;
-	private static void initialPool() {
+    private static JedisPool jedisPool;// 非切片连接池
 
-		if (jedisPool == null) {
-			// 池基本配置
-			JedisPoolConfig config = new JedisPoolConfig();
-			config.setMaxTotal(2000);
-			config.setMaxIdle(50);
-			config.setTestOnBorrow(false);
+    private static void initialPool() {
 
-			jedisPool = new JedisPool(config, ConfigManager.getProperty("redis_server"), 6379);
-			jedis = jedisPool.getResource();
-		}
-	}
+        if (jedisPool == null) {
+            // 池基本配置
+            JedisPoolConfig config = new JedisPoolConfig();
+            config.setMaxTotal(2000);
+            config.setMaxIdle(50);
+            config.setTestOnBorrow(false);
 
+            jedisPool = new JedisPool(config, ConfigManager.getProperty("redis_server"), 6379);
+        }
+    }
 
-	public static String get(String key) {
-		initialPool();
+    public static String get(String key) {
+        initialPool();
 
-		try {
-			String value = jedis.get(key);
-			return value;
-		} catch (Exception e) {
-			logger.error(e);
-			initialPool();
-		}
+        try {
+            Jedis jedis = jedisPool.getResource();
 
-		return null;
+            String value = jedis.get(key);
+            jedis.close();
+            return value;
+        } catch (Exception e) {
+            logger.error(e);
+            initialPool();
+        }
 
-	}
+        return null;
 
-	public static Integer getInt(String key) {
-		String value = get(key);
+    }
 
-		if (EweblibUtil.isValid(value)) {
-			return EweblibUtil.getInteger(value, 0);
-		}
-		return null;
-	}
+    public static Integer getInt(String key) {
+        String value = get(key);
 
-	public static Long getLong(String key) {
-		String value = get(key);
+        if (EweblibUtil.isValid(value)) {
+            return EweblibUtil.getInteger(value, 0);
+        }
+        return null;
+    }
 
-		if (EweblibUtil.isValid(value)) {
+    public static Long getLong(String key) {
+        String value = get(key);
 
-			return EweblibUtil.getLong(value);
-		}
-		return null;
-	}
+        if (EweblibUtil.isValid(value)) {
 
-	public static void set(String key, Object value) {
-		set(key, value, 5 * 60);
-	}
+            return EweblibUtil.getLong(value);
+        }
+        return null;
+    }
 
-	public static void remove(String key) {
-		initialPool();
-		try {
-			jedis.del(key);
-		} catch (Exception e) {
-			logger.error(e);
-			initialPool();
-		}
-	}
+    public static void set(String key, Object value) {
+        set(key, value, 5 * 60);
+    }
 
-	public static void set(String key, Object value, int seconds) {
-		initialPool();
-		if (value != null) {
+    public static void remove(String key) {
+        initialPool();
+        try {
+            Jedis jedis = jedisPool.getResource();
+            jedis.del(key);
+            jedis.close();
+        } catch (Exception e) {
+            logger.error(e);
+            initialPool();
+        }
+    }
 
-			try {
-				jedis.set(key, value.toString());
-				jedis.expire(key, seconds);
-			} catch (Exception e) {
-				logger.error(e);
-				initialPool();
-			}
-		}
-	}
+    public static void set(String key, Object value, int seconds) {
+        initialPool();
+        if (value != null) {
+
+            try {
+                Jedis jedis = jedisPool.getResource();
+                jedis.set(key, value.toString());
+                jedis.expire(key, seconds);
+                jedis.close();
+            } catch (Exception e) {
+                logger.error(e);
+                initialPool();
+            }
+        }
+    }
 
 }
